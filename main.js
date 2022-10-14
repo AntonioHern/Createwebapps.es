4d0e529ca53179ef37082c18e29c1e18670030a6
antoniohernandez <antoniohernba@gmail.com>
2022-10-15 00:48:41 +0200

    alerta

---
+++ main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+    console.log("ready!");
+    $('#alerta').hide();
+
+    $('#enviar').click(function (e) {
+        e.preventDefault();
+        console.log("click");
+        var email = $('#text_email').val();
+        var nombre = $('#text_nombre').val();
+        var mensaje = $('#text_mensaje').val();
+        if (email == '' || nombre == '' || mensaje == '') {
+           console.log("error");
+              $('#alerta').show();
+        } else {
+            $.ajax({
+                url: 'enviar.php',
+                type: 'POST',
+                data: {
+                    email: email,
+                    nombre: nombre,
+                    mensaje: mensaje
+                },
+                success: function (data) {
+                    alert(data);
+                }
+            });
+        }
+    });
+
+    $('#ok').click(function (e) {
+        e.preventDefault();
+        $('#alerta').hide();
+    })
+
+});
